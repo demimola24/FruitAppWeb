8881e699f4b752a7f9f7cdc107f37cabb0d68395
demimola24 <trideks0331>
2023-12-08 17:56:01 -0500

    initial commit and project setup

---
+++ test/widget_test.dart
@@ -0,0 +1,50 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fruitshopweb/data/model/request/auth_request.dart';
+import 'package:fruitshopweb/data/model/response/auth_reponse.dart';
+import 'package:fruitshopweb/data/model/response/fruit_model.dart';
+import 'package:fruitshopweb/data/repository/repository.dart';
+
+void main(){
+  test("login() should return an successful auth response", () {
+    DataSource dataSource = MockRemoteDateSourceSuccess();
+    final result = dataSource.login(AuthRequest());
+    expect(result, isInstanceOf<AuthResponse>());
+  });
+
+  test("signup() should return an successful auth response", () {
+    DataSource dataSource = MockRemoteDateSourceSuccess();
+    final result = dataSource.register(AuthRequest());
+    expect(result, isInstanceOf<AuthResponse>());
+  });
+
+  test("getFruits() should return an successful auth response", () {
+    DataSource dataSource = MockRemoteDateSourceSuccess();
+    final result = dataSource.getFruits();
+    expect(result, isInstanceOf<FruitList>());
+  });
+}
+
+
+class MockRemoteDateSourceSuccess extends DataSource {
+
+  @override
+  Future<FruitList> getFruits() {
+    return Future.value(FruitList(fruitList: []));
+  }
+
+  @override
+  Future<AuthResponse> login(AuthRequest request) {
+    return Future.value(AuthResponse(message: "Mock Result"));
+  }
+
+  @override
+  Future<AuthResponse> register(AuthRequest request) {
+    return Future.value(AuthResponse(message: "Mock Result"));
+  }
+}

a4035cfcfd28eb52d97420b090097694ed7a3ce4
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-12-08 19:40:54 -0500

    In this commit, I've created the ImagePath class to centralize image paths used in the application. The class includes constants for various images, such as landing pages and logos. This structure improves code organization and makes it easier to manage and locate image assets within the project.

---
+++ constants/imagepath.dart
@@ -0,0 +1,15 @@
+
+
+class ImagePath {
+  //images
+  static const String _imageDir = "lib/assets/images";
+  static const String LANDING_IMAGE = "$_imageDir/landing_one.png";
+  static const String LANDING_TWO_IMAGE = "$_imageDir/landing_two.png";
+  static const String LANDING_THREE_IMAGE = "$_imageDir/landing_three.png";
+  static const String LOGO = "$_imageDir/fruit.png";
+  //package_box.png
+
+  //card_image
+
+
+}
